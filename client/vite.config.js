5f895ae5b83afa00a9d70a7205740f579fa6d6b3
noah berlove <noah.berlove@gmail.com>
2023-02-01 11:28:33 -0800

    updates for hosting

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  proxy: {
+    '/': {
+         target: 'https://localhost:9000/',
+         changeOrigin: true,
+         secure: false,      
+         ws: true,
+     }
+}
 })
